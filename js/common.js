7a4fef52294c8645bae60a38fa0b9d5923dd5d76
kimsungrae <rlatjdfo0601@naver.com>
2022-10-25 15:22:53 +0900

    달력

--- js/common.js
+++ js/common.js
@@ -76,6 +76,73 @@ $('input[name="srchchk"]').click(function(){
 
 
 
+   $(function() {
+       //input을 datepicker로 선언
+       $("#datepicker").datepicker({
+        nextText: '다음 달',
+		prevText: '이전 달',
+		showOtherMonths: true,
+        showMonthAfterYear: true,
+		yearSuffix: '년',
+		dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+		monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+		dateFormat: 'yy-mm-dd',
+		onClose: function( selectedDate ) {    
+			//시작일(startDate) datepicker가 닫힐때
+			//종료일(endDate)의 선택할수있는 최소 날짜(minDate)를 선택한 시작일로 지정
+			$('#pickDate').datepicker('option', 'minDate', selectedDate);
+		} 
+       });                    
+       
+       //초기값을 오늘 날짜로 설정해줘야 합니다.
+       $('#datepicker').datepicker('setDate', 'today'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)          
+       
+       $('#startDate').datepicker({
+		nextText: '다음 달',
+		prevText: '이전 달',
+		showOtherMonths: true,
+        showMonthAfterYear: true,
+		yearSuffix: '년',
+		dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+		monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+		dateFormat: 'yy.mm.dd',
+		onClose: function( selectedDate ) {    
+			//시작일(startDate) datepicker가 닫힐때
+			//종료일(endDate)의 선택할수있는 최소 날짜(minDate)를 선택한 시작일로 지정
+			$('#startDate').datepicker('option', 'minDate', selectedDate);
+		},
+		showOn: 'both',
+		buttonImage: 'images/icon_calr.png',
+		buttonImageOnly: true
+	});
+	$('#endDate').datepicker({
+		nextText: '다음 달',
+		prevText: '이전 달',
+		showOtherMonths: true,
+        showMonthAfterYear: true,
+		yearSuffix: '년',
+		dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+		monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+		dateFormat: 'yy.mm.dd',
+		onClose: function( selectedDate ) {    
+			//시작일(startDate) datepicker가 닫힐때
+			//종료일(endDate)의 선택할수있는 최소 날짜(minDate)를 선택한 시작일로 지정
+			$('#endDate').datepicker('option', 'minDate', selectedDate);
+		},
+		showOn: 'both',
+		buttonImage: 'images/icon_calr.png',
+		buttonImageOnly: true
+	});
+
+   });
+
+
 
 
 
